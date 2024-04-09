b9738740a54105a3b8401e40e66d1c26fb400394
PriyaMandavia <158045315+PriyaMandavia@users.noreply.github.com>
2024-04-09 15:59:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,7 @@ class Settings():
     SECRET_KEY = os.environ.get('SECRET_KEY',None)
     ALGORITHM = os.environ.get('ALGORITHM',None)
     ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES',None)
-    REFRESH_TOKEN_EXPIRE_DAYS = os.environ.get('REFRESH_TOKEN_EXPIRE_DAYS',None)
-    EMAIL_HOST=os.environ.get('EMAIL_HOST' , None)
-    EMAIL_PORT=os.environ.get('EMAIL_PORT' , None)
-    EMAIL_USERNAME=os.environ.get('EMAIL_USERNAME' , None)
-    EMAIL_PASSWORD=os.environ.get('EMAIL_PASSWORD' ,None)
-    EMAIL_FROM=os.environ.get('EMAIL_FROM' , None)
-
+    
 
     class Config:
         env_file = './.env'
